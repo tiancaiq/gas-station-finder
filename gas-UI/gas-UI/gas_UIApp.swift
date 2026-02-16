b5370ac08a41191d41f47bef02cd9dc4ec4007b7
tiancaiq <146501425+tiancaiq@users.noreply.github.com>
2026-02-15 17:52:31 -0800

    Add files via upload
    gasui

---
+++ gas-UI/gas-UI/gas_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  gas_UIApp.swift
+//  gas-UI
+//
+//  Created by B Shen on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct gas_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gas-UI/gas-UITests/gas_UITests.swift
@@ -0,0 +1,17 @@
+//
+//  gas_UITests.swift
+//  gas-UITests
+//
+//  Created by B Shen on 2/14/26.
+//
+
+import Testing
+@testable import gas_UI
+
+struct gas_UITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
